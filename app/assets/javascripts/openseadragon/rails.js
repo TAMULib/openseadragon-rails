7d0e295c1bb9ebcb02f500a0ebd95b2891577a5c
William Welling <wwelling@library.tamu.edu>
2016-11-16 10:26:14 -0600

    Removed empty line. Added comment

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -5,11 +5,12 @@
     $('picture[data-openseadragon]').openseadragon();
   }
 
-
   $(document).on('page:load', initOpenSeadragon);
   $(document).ready(initOpenSeadragon);
 
-  console.log('commented out Turbolinks');
+  // This fix incurs duplicate openseadragons to open on the page
+  // https://github.com/projectblacklight/spotlight/issues/1630
+  //console.log('commented out Turbolinks');
   // var handler = 'ready';
   // if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
   //   // Turbolinks 5
