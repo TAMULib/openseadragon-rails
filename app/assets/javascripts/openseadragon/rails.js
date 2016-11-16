926661dbec52a66c205d1a64d2115688ffc43d3a
William Welling <wwelling@library.tamu.edu>
2016-11-16 09:56:46 -0600

    Disable turbolinks fix

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -5,19 +5,20 @@
     $('picture[data-openseadragon]').openseadragon();
   }
 
-  console.log('removed additional initOpenSeadragon');
-  //$(document).on('page:load', initOpenSeadragon);
-  //$(document).ready(initOpenSeadragon);
 
-  var handler = 'ready';
-  if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-    // Turbolinks 5
-    if (Turbolinks.BrowserAdapter) {
-      handler = 'turbolinks:load';
-    } else {
-      // Turbolinks < 5
-      handler = 'page:load ready';
-    }
-  }
-  $(document).on(handler, initOpenSeadragon);
+  $(document).on('page:load', initOpenSeadragon);
+  $(document).ready(initOpenSeadragon);
+
+  console.log('commented out Turbolinks');
+  // var handler = 'ready';
+  // if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+  //   // Turbolinks 5
+  //   if (Turbolinks.BrowserAdapter) {
+  //     handler = 'turbolinks:load';
+  //   } else {
+  //     // Turbolinks < 5
+  //     handler = 'page:load ready';
+  //   }
+  // }
+  // $(document).on(handler, initOpenSeadragon);
 })(jQuery);
